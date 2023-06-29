bf5f5e09c66628693fb57c779cc5cc37b8dc3d6b
Junior DUSSOUILLEZ <junior.dussouillez@gmail.com>
2023-06-29 09:21:44 +0200

    feat: add test endpoint

--- src/main/java/org/acme/getting/started/ReactiveGreetingResource.java
+++ src/main/java/org/acme/getting/started/ReactiveGreetingResource.java
@@ -1,46 +1,26 @@
 package org.acme.getting.started;
 
-import jakarta.inject.Inject;
+import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
-
-import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
-import org.jboss.resteasy.reactive.RestSseElementType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello")
 public class ReactiveGreetingResource {
 
-    @Inject
-    ReactiveGreetingService service;
-
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
-    public Uni<String> greeting(String name) {
-        return service.greeting(name);
-    }
-
-    @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @Path("/greeting/{count}/{name}")
-    public Multi<String> greetings(int count, String name) {
-        return service.greetings(count, name);
-    }
-
-    @GET
-    @Produces(MediaType.SERVER_SENT_EVENTS)
-    @RestSseElementType(MediaType.TEXT_PLAIN)
-    @Path("/stream/{count}/{name}")
-    public Multi<String> greetingsAsStream(int count, String name) {
-        return service.greetings(count, name);
-    }
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public Uni<Response> greeting(
+        @PathParam("name") final String name,
+        @QueryParam("status") final int status) {
+        if (status == 200) {
+            return Uni.createFrom().item(Response.ok("Hello " + name).build());
+        }
+        return Uni.createFrom().item(Response.status(status).build());
     }
 }
